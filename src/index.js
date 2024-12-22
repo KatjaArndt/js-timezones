5bb0a62a780a3ae2ef1a296fdaf1c363d476fe38
Katja <arndt-katja@outlook.de>
2024-12-22 21:14:42 +0100

    challenge 1 + 2

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/*Challenge 2*/
+let dateHonolulu = document.querySelector("#current-date");
+dateHonolulu.innerHTML = moment.tz("Pacific/Honolulu"); /*Format noch ändern*/
+
+/*Challenge 3*/
+/*Challenge 4*/
