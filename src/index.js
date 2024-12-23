60b6ce1e6ffa5319dcc4c979b8a7e2fdc2863124
Katja <arndt-katja@outlook.de>
2024-12-23 13:35:12 +0100

    Challenges fertiggestellt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 /*Challenge 2*/
 let dateHonolulu = document.querySelector("#current-date");
-dateHonolulu.innerHTML = moment.tz("Pacific/Honolulu"); /*Format noch ändern*/
+let formattedDate = moment
+  .tz("Pacific/Honolulu")
+  .format("dddd, D MMMM YYYY, h:mm A");
+dateHonolulu.innerHTML = `It is ${formattedDate} in Honolulu, Hawai`;
 
 /*Challenge 3*/
+let dateParis = document.querySelector("#future-date");
+let formattedDateParis = moment
+  .tz("Pacific/Honolulu")
+  .add(10, "years")
+  .format("dddd, D MMMM, YYYY, h:mm A");
+dateParis.innerHTML = `10 years from now, it will be ${formattedDateParis} in Paris, France`;
+
 /*Challenge 4*/
+
+let localTimezone = document.querySelector("#local-time-zone");
+localTimezone.innerHTML = moment.tz.guess();
